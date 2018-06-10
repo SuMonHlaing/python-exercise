3d4822ea740dcff9496b1b620a3b8180a5d1778e
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-10 23:05:56 +0630

    python ex34

---
+++ ex34.py
@@ -0,0 +1,17 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+python = animals[1]
+peacock = animals[2]
+kangaroo = animals[3]
+whale = animals[4]
+platypus = animals[5]
+
+print(f"The first animal is at 0 and is a {bear}")
+print(f"The third animal is at 2 and is a {peacock}")
+print(f"The first animal is {bear}")
+print(f"The animal at 3 is {peacock}")
+print(f"The fifth animal is {whale}")
+print(f"The animal at 2 is {python}")
+print(f"The sixth animal is {platypus}")
+print(f"The animal at 4 is {kangaroo}")
+
