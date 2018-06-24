fb202e83f2d6cd78072ef0304921600fe07c960f
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-24 17:28:26 +0630

    python ex35

--- ex35.py
+++ ex35.py
@@ -1,19 +1,20 @@
 from sys import exit
 
+
 def gold_room():
     print("This room is full of gold. How much do you take?")
 
-    choice = input ("> ")
+    choice = input("> ")
     if "0" in choice or "1" in choice:
         how_much = int(choice)
     else:
         dead("Man, learn to type a number.")
 
-        if how_much < 50:
-            print("Nice, you're not greedy, you win!")
-            exit(0)
-        else:
-            dead("You greedy bastardsd!")
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
 
 
 def bear_room():
@@ -27,9 +28,9 @@ def bear_room():
         choice = input("> ")
 
         if choice == "take honey":
-            dead("The bear looks at you then slaps your face")
+            dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
-            print("The bear has moved form the door.")
+            print("The bear has moved from the door.")
             print("You can go through it now.")
             bear_moved = True
         elif choice == "taunt bear" and bear_moved:
@@ -58,6 +59,7 @@ def cthulhu_room():
 def dead(why):
     print(why, "Good job!")
     exit(0)
+
 
 def start():
     print("You are in a dark room.")
