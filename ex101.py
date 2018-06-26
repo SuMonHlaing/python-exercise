7cca9d80685497632c46ccbb4e46ec1f9cec01d7
Ma Su Mon Hlaing <39287902+SuMonHlaing@users.noreply.github.com>
2018-06-26 10:03:57 +0630

    cat to pet

--- ex101.py
+++ ex101.py
@@ -1,12 +1,12 @@
 tabby_pet = "\tI'm tabbed in."
 persian_pet = "I'm splilt\non a line."
-backslash_pet = "I'm \\ a \\ dog."
+backslash_pet = "I'm \\ a \\ pet."
 
 fat_pet = """
 I'll do a list:
-\t* Cat food
+\t* Pet food
 \t* Fishies
-\t* Catnip\n\t* Grass
+\t* Petnip\n\t* Grass
 """
 
 print (tabby_pet)
