94ad742b724b22d3ee159dd5485a33a3614c3ea1
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-28 14:46:07 +0630

    python ex5

--- ex5.py
+++ ex5.py
@@ -13,5 +13,3 @@ print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total} .")
-
-
